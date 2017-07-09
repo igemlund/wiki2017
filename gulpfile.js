de189d955a20629cd3c99669f13679e09d9990f8
Marcus Rettig <marcus0706@live.se>
2017-07-09 17:00:03 +0200

    Browsersync host pages

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,18 @@ const gulp = require('gulp');
 const replace = require('gulp-replace');
 const wrap = require('gulp-wrap');
 const fs = require('fs');
+const browserSync = require('browser-sync');
+
+const config = {
+  TeamName: 'Lund',
+};
 
 const paths = {
   appDir: 'app',
-  tempDir: 'temp',
-  pages: `app/pages/**/*.html`,
-  templatesDir: `app/templates`,
+  distDir: 'dist',
+  pages: 'app/pages/**/*.html',
+  templatesDir: 'app/templates',
+  distPagesDir: 'dist/pages',
 };
 
 const wrapper = `
@@ -29,5 +35,29 @@ gulp.task('pages', () => {
       return template;
     }))
     .pipe(wrap(wrapper))
-    .pipe(gulp.dest(paths.tempDir));
+    .pipe(gulp.dest(paths.distDir));
 });
+
+gulp.task('build', ['pages']);
+
+gulp.task('browser-sync', ['pages'], () => {
+  const { TeamName } = config;
+  const routes = {};
+  routes[`/Team:${TeamName}`] = paths.distPagesDir;
+
+  browserSync.init({
+    server: {
+      baseDir: paths.distDir,
+      routes,
+      middleware: (req,res,next) => {
+        if (!(/\.[0-9a-z]+$/.test(req.url))) {
+          // Use .html as default extension if none is provided
+          req.url = `${req.url}.html`;
+        }
+        return next();
+      },
+    },
+  });
+});
+
+gulp.task('serve', ['build', 'browser-sync']);
